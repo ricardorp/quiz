0c4b9fcfbdb73cdc37e57cbbe4b151e830387d6f
Ricardo Rodríguez <ricardorp@gmail.com>
2015-07-09 07:44:02 +0200

    Múltiples preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,28 +2,36 @@ var author = 'Ricardo Rodríguez',
     title = 'Quiz'
     models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-    models.Quiz.findAll().success(function(quiz){
-        res.render('quizes/question', {
+// GET /quizes
+exports.index = function(req, res) {
+    models.Quiz.findAll().then(function(quizes){
+        res.render('quizes/index.ejs', {quizes : quizes});
+    });
+}
+// GET /quizes/:id
+exports.show = function(req, res) {
+    models.Quiz.find(req.params.quizId).then(function(quiz){
+        res.render('quizes/show', {
             title: title,
-            pregunta: quiz[0].pregunta
+            quiz: quiz
         });
     });
 }
 
-// GET /quizes/answer
+// GET /quizes/:id/answer
 exports.answer = function (req, res) {
-    models.Quiz.findAll().success(function (quiz) {
-        if (req.query.respuesta === quiz[0].respuesta) {
+    models.Quiz.find(req.params.quizId).then(function (quiz) {
+        if (req.query.respuesta === quiz.respuesta) {
             res.render('quizes/answer', {
-                title: title,
-                respuesta: 'Correcto'
+                title : title,
+                quiz : quiz,
+                respuesta : 'Correcto'
             });
         } else {
             res.render('quizes/answer', {
-                title: 'Quiz',
-                respuesta: 'Incorrecto'
+                title : title,
+                quiz : quiz,
+                respuesta : 'Incorrecto'
             });
         }
     });
--- models/models.js
+++ models/models.js
@@ -33,7 +33,8 @@ sequelize.sync().success(function () {
     // success(..) ejecuta el manejador una vez creada la tabla
     Quiz.count().success(function (count) {
         if (count === 0) {
-            Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'}).success(function () {
+            Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'});
+            Quiz.create({pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}).then(function () {
                 console.log('Base de datos inicializada.');
             });
         }
