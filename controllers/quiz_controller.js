e7f9b2c2739c08f2191241c8eb7fc11bf8bb3895
Ricardo Rodríguez <ricardorp@gmail.com>
2015-07-10 20:17:21 +0200

    Añadida la búsqueda de preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,6 @@
 var author = 'Ricardo Rodríguez',
-    title = 'Quiz'
-models = require('../models/models.js');
+    title = 'Quiz',
+    models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function (req, res, next, quizId) {
@@ -21,21 +21,25 @@ exports.load = function (req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function (req, res) {
-    models.Quiz.findAll().then(
+    var search = _procesarSearch(req.query.search),
+        whereSection = search ? {where: ["upper(pregunta) like ?", search]} : {};
+
+    models.Quiz.findAll(whereSection).then(
         function (quizes) {
             res.render('quizes/index.ejs', {quizes: quizes});
         }
     ).catch(function (error) {
-            next(error)
+            next(error);
         });
-}
+};
+
 // GET /quizes/:id
 exports.show = function (req, res) {
     res.render('quizes/show', {
         title: title,
         quiz: req.quiz
     });
-}
+};
 
 // GET /quizes/:id/answer
 exports.answer = function (req, res) {
@@ -48,9 +52,23 @@ exports.answer = function (req, res) {
         quiz: req.quiz,
         respuesta: resultado
     });
-}
+};
 
 // GET /author
 exports.author = function (req, res) {
     res.render('author', {author: author});
-}
+};
+
+/**
+ * Procesa una query de búsqueda para transformarla en una query SQL. Si la query no existe o está vacía, devuelve null.
+ * @param query Query de búsqueda compuesta por una o más palabras.
+ * @returns {String} Si la query no existe o está vacía devuelve null, en otro caso, devuelve cada palabra pasada a
+ * mayúsculas y separada por un %. Ejemplo Capital Italia se convertiría en %CAPITAL%ITALIA%
+ * @private
+ */
+_procesarSearch = function (query) {
+    if (query) {
+        return query.trim().replace(/(\w+)/g, '%$1%').replace(/\s+/g, '').replace(/%+/g, '%').toUpperCase()
+    }
+    return null;
+};
