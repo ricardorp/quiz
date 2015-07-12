6f75d86583c0ce5f86171d2384c3c5e9a041d0e4
Ricardo Rodríguez <ricardorp@gmail.com>
2015-07-12 20:11:58 +0200

    Se añade soporte para la creación de preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,6 +2,26 @@ var author = 'Ricardo Rodríguez',
     title = 'Quiz',
     models = require('../models/models.js');
 
+// GET /quizes/new
+exports.new = function (req, res) {
+    var quiz = models.Quiz.build({
+        pregunta: 'Pregunta',
+        respuesta: 'Respuesta'
+    });
+    res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function (req, res) {
+    var quiz = models.Quiz.build(req.body.quiz);
+    // guarda en BD los campos pregunta y respuesta de quiz
+    quiz.save({
+        fields: ['pregunta', 'respuesta']
+    }).then(function () {
+        res.redirect('/quizes');
+    }); // Redirección HTTP a la lista de preguntas.
+};
+
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function (req, res, next, quizId) {
     models.Quiz.find(quizId).then(
